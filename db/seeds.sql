3be1605d1ee4cc19ad09628da50743b3dace2aa2
Cyrus Jose <cyrusjose@Cyruss-Mac-mini.local>
2020-08-08 15:50:22 -0700

    Created database and tested with seeds

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Impossible burger');
+INSERT INTO burgers (burger_name) VALUES ('The Clogger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Vegan Burger');
