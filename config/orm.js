3c9e2c5f545bb92dd13f90fb46001541766edf96
Cyrus Jose <cyrusjose@Cyruss-Mac-mini.local>
2020-08-08 16:47:22 -0700

    Using logic from cats class example

---
+++ config/orm.js
@@ -0,0 +1,27 @@
+var connection = require('../config/connection');
+var printQuestionMarks = (num) => {
+    var arr = [];
+
+    for (vari = 0; i < num; i++) {;
+        arr.push('?')
+    };
+    return arr.toString();;
+};
+
+function objToSql(ob) {
+    var arr = [];
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === 'string' && value.indexOf(' ') >= 0) {
+                value = "'" + value + "'";
+            };
+            arr.push(key + '=' + value);
+        };
+    };
+    return arr.toString();
+};
+
+var orm = {
+
+}
