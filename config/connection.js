eb1fae126e7d7727c5dd1b88034140998caf64b9
Cyrus Jose <cyrus.jose@outlook.com>
2020-08-08 17:41:32 -0700

    Clean up

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,19 @@
 varmysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db",
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "burgers_db",
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.log("error connecting " + err.stack);
-        return;
-    }
-    console.log('connected as id' + connection.threadId);
-
+  if (err) {
+    console.log("error connecting " + err.stack);
+    return;
+  }
+  console.log("connected as id" + connection.threadId);
 });
 
 module.exports = connection;
