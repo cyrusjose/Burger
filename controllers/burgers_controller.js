1c38c12d271bfa730f01de05bfe67be8825b61f7
Cyrus Jose <cyrus.jose@outlook.com>
2020-08-08 18:36:00 -0700

    Added code to get the objects from the user

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,13 @@
+var router = express.Router();
+
+var burger = require("../models/burger");
+
+router.get("/", function (req, res) {
+  burger.selectAll(function (data) {
+    var hbsObject = {
+      burgers: data,
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
