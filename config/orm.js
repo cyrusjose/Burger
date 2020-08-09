a2e8862f13731661f9a903b8105527735efb7a52
Cyrus Jose <cyrus.jose@outlook.com>
2020-08-08 17:56:40 -0700

    Testing connection

--- config/orm.js
+++ config/orm.js
@@ -1,27 +1,25 @@
-var connection = require('../config/connection');
+var connection = require("../config/connection");
 var printQuestionMarks = (num) => {
-    var arr = [];
+  var arr = [];
 
-    for (vari = 0; i < num; i++) {;
-        arr.push('?')
-    };
-    return arr.toString();;
+  for (vari = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
 };
 
 function objToSql(ob) {
-    var arr = [];
-    for (var key in ob) {
-        var value = ob[key];
-        if (Object.hasOwnProperty.call(ob, key)) {
-            if (typeof value === 'string' && value.indexOf(' ') >= 0) {
-                value = "'" + value + "'";
-            };
-            arr.push(key + '=' + value);
-        };
-    };
-    return arr.toString();
-};
-
-var orm = {
-
+  var arr = [];
+  for (var key in ob) {
+    var value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+  return arr.toString();
 }
+
+var orm = {};
